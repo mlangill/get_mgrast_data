68430928059ef0fa0c51d895bb986f6d94233367
Morgan Langille <morgan.g.i.langille@gmail.com>
2014-01-13 13:36:54 -0400

    Add more options for metadata
    1) Change default metadata to 'None'.
    2) Add another STAMP "level" that contains the observation id as the lowest level.
    3) If BIOM file doesn't have any observation metadata then just a single level with containing the observation id is used (same as basic biom to tab conversion)
    4) Add a few usage cases illustrating how to use script with OTU table or function table.

--- biom_to_stamp.py
+++ biom_to_stamp.py
@@ -14,18 +14,22 @@ from cogent.util.option_parsing import parse_command_line_parameters, make_optio
 from biom.parse import parse_biom_table
 from os.path import join,splitext
 import gzip
-
+import sys
 script_info = {}
 script_info['brief_description'] = "Convert a BIOM table to a compatible STAMP profile table."
 script_info['script_description'] = "Metadata will be parsed and used as hiearachal data for STAMP."
 
 script_info['script_usage'] = [\
-("Minimum Requirments","A BIOM file. ","%prog table1.biom > table1.spf")]
+("Minimum Requirments","","%prog table1.biom > table1.spf"),
+("OTU table from QIIME","","%prog -m taxonomy table1.biom > table1.spf"),
+("Function table from PICRUSt","","%prog -m KEGG_Pathways table1.biom > table1.spf"),
+("Function table from MG-RAST","","%prog -m ontology table1.biom > table1.spf")
+]
 
 script_info['output_description']= "Output is written to STDOUT"
 
 script_info['optional_options'] = [\
-    make_option('-m','--metadata',default='taxonomy',type="string",help='Name of metadata. [default: %default]')]
+    make_option('-m','--metadata',default=None,type="string",help='Name of metadata. [default: %default]')]
 
 
 script_info['disallow_positional_arguments'] = False
@@ -52,13 +56,18 @@ def main():
 
     metadata_name=opts.metadata
 
-    #figure out the longest list within the given metadata
-    max_len_metadata = max(len(p[metadata_name]) for p in table.ObservationMetadata)
+    if metadata_name is None:
+        max_len_metadata=0
+    elif table.ObservationMetadata and metadata_name in table.ObservationMetadata[0]:
+        #figure out the longest list within the given metadata
+        max_len_metadata = max(len(p[metadata_name]) for p in table.ObservationMetadata)
+    else:
+        raise ValueError("'"+metadata_name+"' was not found in the BIOM table. Please try changing --metadata to a valid metadata field.")
 
     #make the header line
     header=[]
-    #make simple labels for each level in the metadata (e.g. 'Level_1', 'Level_2', etc.)
-    for i in range(max_len_metadata):
+    #make simple labels for each level in the metadata (e.g. 'Level_1', 'Level_2', etc.) "+1" for the observation id as well.
+    for i in range(max_len_metadata+1):
         header.append('Level_'+ str(i+1))
     
     #add the sample ids to the header line
@@ -68,12 +77,17 @@ def main():
 
     #now process each observation (row in the table)
     for obs_vals,obs_id,obs_metadata in table.iterObservations():
-        row=obs_metadata[metadata_name]
+        row=[]
+        if max_len_metadata >0:
+            row=obs_metadata[metadata_name]
         
         #Add blanks if the metadata doesn't fill each level
         if len(row) < max_len_metadata:
             for i in range(max_len_metadata - len(row)):
                 row.append('')
+
+        #Add the observation id as the last "Level"
+        row.append(obs_id)
 
         #Add count data to the row
         row.extend(map(str,obs_vals))
